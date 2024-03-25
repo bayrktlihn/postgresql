03dd6dc60e3bf33af87f80534b9c01e033a7e43b
bayrktlihn <36965493+bayrktlihn@users.noreply.github.com>
2024-03-26 00:27:35 +0300

    Create factorial.sql

---
+++ factorial.sql
@@ -0,0 +1,18 @@
+CREATE OR replace FUNCTION fn_factorial(p_num bigint)
+RETURNS bigint LANGUAGE plpgsql AS $$
+DECLARE
+	result bigint;
+BEGIN
+	
+	if p_num < 0 then
+		raise exception 'invalid param';
+	end if;
+
+	if p_num = 0 or p_num = 1 then 
+		return 1;
+	end if;
+
+	return p_num * fn_factorial(p_num - 1);
+	
+END;
+$$
